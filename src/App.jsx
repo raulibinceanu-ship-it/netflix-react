38eadbf6aa99fe4a49585cfffd9d354d1b3660c2
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2025-11-26 14:07:50 +0100

    route1st modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,35 @@
-import "./App.css";
 import MyNavbar from "./components/MyNavbar";
 import MyFooter from "./components/MyFooter";
 import MovieGallery from "./components/MovieGallery";
+
+import TVShows from "./components/TVShows";
+import MovieDetails from "./components/MovieDetails";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <MyNavbar />
-      <div className="container-fluid px-4 mt-4">
-        <div className="d-flex justify-content-between align-items-center mb-3">
-          <div className="d-flex align-items-center">
-            <h2 className="mb-0">TV Shows</h2>
 
-            <div className="dropdown ms-4">
-              <button
-                type="button"
-                className="btn btn-secondary btn-sm dropdown-toggle rounded-0"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style={{ backgroundColor: "#221f1f", borderColor: "white" }}
-              >
-                Genres
-              </button>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Comedy
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Drama
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Thriller
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <MovieGallery title="Harry Potter" query="harry potter" />
+              <MovieGallery title="Star Wars" query="star wars" />
+              <MovieGallery title="Spider Man" query="spider man" />
+            </>
+          }
+        />
 
-          <div>
-            <i className="bi bi-grid icons"></i>
-            <i className="bi bi-grid-3x3 icons"></i>
-          </div>
-        </div>
+        {/* ROTTA TV SHOWS */}
+        <Route path="/tv-shows" element={<TVShows />} />
 
-        {/* 3 GALLERIE prese da OMDb */}
-        <MovieGallery title="Trending Now" query="Harry Potter" />
-        <MovieGallery title="Watch it Again" query="Star Wars" />
-        <MovieGallery title="New Releases" query="Lord of the Rings" />
-      </div>
+        {/* ROTTA PARAMETRI */}
+        <Route path="/movie-details/:movieId" element={<MovieDetails />} />
+      </Routes>
 
       <MyFooter />
     </>
